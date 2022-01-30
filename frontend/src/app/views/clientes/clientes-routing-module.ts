d18922c6468b1d16edaf412ecc4f3359d3ac849b
Jonathan Dyallo <emeraldusk@gmail.com>
2022-01-30 13:03:18 -0300

    feat: clientes

---
+++ frontend/src/app/views/clientes/clientes-routing-module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { ClientesComponent } from './clientes.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: ClientesComponent,
+    data: {
+      title: 'Clientes'
+    }
+  },
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class ClientesRoutingModule { }
