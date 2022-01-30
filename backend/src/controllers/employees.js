2ad7490eaa5344b9d66cb7a3f2dfbec2db24de12
Jonathan Dyallo <emeraldusk@gmail.com>
2022-01-30 13:15:48 -0300

    feat: added cors, try/catch, fixed errors

--- backend/src/controllers/employees.js
+++ backend/src/controllers/employees.js
@@ -2,36 +2,58 @@ const employeeController = {};
 const Employee = require("../models/employees");
 
 employeeController.getEmployees = async (req, res) => {
-  const employees = await Employee.find();
-  res.json(employees);
+  try {
+    const employees = await Employee.find();
+    res.json(employees);
+  } catch (error) {
+    console.log(error);
+  }
 };
+
 employeeController.getEmployee = async (req, res) => {
-  const employee = await Employee.findById(req.params.id);
-  res.json(employee);
+  try {
+    const employee = await Employee.findById(req.params.id);
+    res.json(employee);
+  } catch (error) {
+    console.log(error);
+  }
 };
+
 employeeController.updateEmployee = async (req, res) => {
-  const updateEmployee = await Employee.findByIdAndUpdate(
-    req.params.id,
-    req.body
-  );
-  await updateEmployee.save();
-  res.json({ status: "Employee updated" });
+  const { id } = req.params;
+  try {
+    const updateEmployee = await Employee.findByIdAndUpdate(id, req.body);
+    await updateEmployee.save();
+    res.json({ status: "Employee updated" });
+  } catch (error) {
+    console.log(error);
+  }
 };
+
 employeeController.deleteEmployee = async (req, res) => {
-  await Employee.findOneAndDelete(req.params.id);
-  res.json({ status: "Employee deleted successfully" });
+  const { id } = req.params;
+  try {
+    await Employee.findByIdAndDelete(id);
+    res.json({ status: "Employee deleted successfully" });
+  } catch (error) {
+    console.log(error);
+  }
 };
 employeeController.createEmployee = async (req, res) => {
-  const newEmployee = new Employee({
-    firstName: req.body.firstName,
-    lastName: req.body.lastName,
-    email: req.body.email,
-    phone: req.body.phone,
-    position: req.body.position,
-    salary: req.body.salary,
-  });
-  await newEmployee.save();
-  res.json({ message: "Employee saved successfully" });
+  try {
+    const newEmployee = new Employee({
+      firstName: req.body.firstName,
+      lastName: req.body.lastName,
+      email: req.body.email,
+      phone: req.body.phone,
+      position: req.body.position,
+      salary: req.body.salary,
+    });
+    await newEmployee.save();
+    res.json({ message: "Employee saved successfully" });
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 module.exports = employeeController;
