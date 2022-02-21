750d7460cb0eb89e7add790735aa00c697b99409
jd-apprentice <emeraldusk@gmail.com>
2022-02-21 00:15:09 -0300

    testing more ways to fix production

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 import app from './server';
 import "./database.ts";
+import dotenv from "dotenv";
+dotenv.config();
 app.listen(process.env.PORT || 3000, () => {
     console.log(`Server is running on port ${process.env.PORT}`);
 })
