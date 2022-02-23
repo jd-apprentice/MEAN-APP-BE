4902b746a01534297b47f6ec5b0386aba217bfa1
jd-apprentice <emeraldusk@gmail.com>
2022-02-23 01:45:45 -0300

    added random endpoint

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,9 +1,11 @@
 import { Router } from 'express';
 import employeesRoutes from './employees.routes';
 import userRoutes from './user.routes';
+import randomRoutes from './random.routes';
 const allRoutes = Router();
 
 allRoutes.use('/employees', employeesRoutes);
 allRoutes.use('/user', userRoutes);
+allRoutes.use('/random', randomRoutes);
 
 export default allRoutes;
